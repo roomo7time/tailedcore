86aab05978664c14dbb848b38271da4f9a30bfaf
jaewoo_park <park.jaewoo@aiv.ai>
2024-02-26 20:45:46 +0900

    feat: easy data for mvtec

--- run_extract_mvtec.sh
+++ run_extract_mvtec.sh
@@ -4,27 +4,23 @@
 # Create logs directory if it doesn't exist
 mkdir -p ./logs
 
-# Define config names
-CONFIG_NAMES=(
-    "tailedpatch_mvtec_01"
-    "tailedpatch_mvtec_05"
-    "tailedpatch_mvtec_06"
-    "tailedpatch_mvtec_07"
-)
+for seed in {10..29}; do
+    # Define config names
+    CONFIG_NAMES=(
+        "aatailedpatch_mvtec_01"
+    )
 
-# Define specific data names with their corresponding seeds
-DATA_NAMES=(
-    "mvtec_step_nr10_tk1_tr60_seed0"
-    "mvtec_step_nr10_tk1_tr60_seed7"
-    "mvtec_step_nr10_tk4_tr60_seed0"
-    "mvtec_step_nr10_tk4_tr60_seed7"
-    "mvtec_pareto_nr10_seed0"
-    "mvtec_pareto_nr10_seed2"
-)
+    # Define specific data names with their corresponding seeds
+    DATA_NAMES=(
+        "mvtec_step_nr10_tk1_tr60_seed${seed}"
+        "mvtec_step_nr10_tk4_tr60_seed${seed}"
+        "mvtec_pareto_nr10_seed0"
+    )
 
-# Process each data name with each config name
-for data_name in "${DATA_NAMES[@]}"; do
-    for config_name in "${CONFIG_NAMES[@]}"; do
-        python extract_artifacts.py --data_sub_path "anomaly_detection/${data_name}" --config_name "${config_name}"
+    # Process each data name with each config name
+    for data_name in "${DATA_NAMES[@]}"; do
+        for config_name in "${CONFIG_NAMES[@]}"; do
+            python extract_artifacts.py --data_sub_path "anomaly_detection/${data_name}" --config_name "${config_name}"
+        done
     done
 done
--- run_model.sh
+++ run_model.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# Define config names
-CONFIG_NAMES=(
-    "atailedpatch_mvtec_01"
-)
+for seed in {10..29}; do
+    # Define config names
+    CONFIG_NAMES=(
+        "aatailedpatch_mvtec_01"
+    )
 
-# Define specific data names with their corresponding seeds
-DATA_NAMES=(
-    "mvtec_step_nr10_tk1_tr60_seed0"
-    "mvtec_step_nr10_tk1_tr60_seed7"
-    "mvtec_step_nr10_tk4_tr60_seed0"
-    "mvtec_step_nr10_tk4_tr60_seed7"
-    "mvtec_pareto_nr10_seed0"
-    "mvtec_pareto_nr10_seed2"
-)
+    # Define specific data names with their corresponding seeds
+    DATA_NAMES=(
+        "mvtec_step_nr10_tk1_tr60_seed${seed}"
+        "mvtec_step_nr10_tk4_tr60_seed${seed}"
+        "mvtec_pareto_nr10_seed0"
+    )
 
-# Process each data name with each config name
-for data_name in "${DATA_NAMES[@]}"; do
-    for config_name in "${CONFIG_NAMES[@]}"; do
-        python evaluate.py --data_sub_path "anomaly_detection/${data_name}" --config_name "${config_name}"
+    # Process each data name with each config name
+    for data_name in "${DATA_NAMES[@]}"; do
+        for config_name in "${CONFIG_NAMES[@]}"; do
+            python evaluate.py --data_sub_path "anomaly_detection/${data_name}" --config_name "${config_name}"
+        done
     done
 done
