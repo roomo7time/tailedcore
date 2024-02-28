f7914e5258ce5d76e9ad73eca392ce8496dbd995
jaewoo_park <park.jaewoo@aiv.ai>
2024-02-28 12:25:38 +0900

    chore: misc

--- run_model.sh
+++ run_model.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-for seed in {400..499}; do
-    # Define config names
+for seed in {444..499}; do
     CONFIG_NAMES=(
         "aatailedpatch_mvtec_01"
     )
 
-    # Define specific data names with their corresponding seeds
     DATA_NAMES=(
         "mvtec_step_hard_nr10_tk4_tr70_seed${seed}"
-        # "mvtec_step_nr10_tk4_tr70_seed${seed}"
-        # "mvtec_pareto_nr10_seed${seed}"
     )
 
-    # Process each data name with each config name
     for data_name in "${DATA_NAMES[@]}"; do
         for config_name in "${CONFIG_NAMES[@]}"; do
             python evaluate.py --data_sub_path "${data_name}" --config_name "${config_name}"
