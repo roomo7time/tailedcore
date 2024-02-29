313e3cab88c2999d9b9e7f24ba07911916dd0d8f
jaewoo_park <park.jaewoo@aiv.ai>
2024-02-29 23:26:34 +0900

    feat: feature change

--- run_model.sh
+++ run_model.sh
@@ -1,36 +1,36 @@
 
 #!/bin/bash
 
-# # mvtec
-# for seed in {50..99}; do
+# mvtec
+for seed in {18..49}; do
 
-#     # python evaluate.py \
-#     #     --data_sub_path "mvtec_pareto_random_nr10_seed${seed}" \
-#     #     --config_name "aatailedpatch_mvtec_02"
-
-#     # python evaluate.py \
-#     #     --data_sub_path "mvtec_step_random_nr10_tk4_tr60_seed${seed}" \
-#     #     --config_name "aatailedpatch_mvtec_01"
-
-#     python evaluate.py \
-#         --data_sub_path "mvtec_step_random_nr10_tk1_tr60_seed${seed}" \
-#         --config_name "aatailedpatch_mvtec_01"
-    
-# done
-
-# visa
-for seed in {0..49}; do
+    # python evaluate.py \
+    #     --data_sub_path "mvtec_pareto_random_nr10_seed${seed}" \
+    #     --config_name "aatailedpatch_mvtec_02"
 
     python evaluate.py \
-        --data_sub_path "visa_pareto_random_nr05_seed${seed}" \
-        --config_name "aatailedpatch_mvtec_05"
+        --data_sub_path "mvtec_step_random_nr10_tk4_tr60_seed${seed}" \
+        --config_name "aatailedpatch_mvtec_01"
 
     # python evaluate.py \
-    #     --data_sub_path "visa_step_random_nr05_tk4_tr60_seed${seed}" \
-    #     --config_name "aatailedpatch_mvtec_01"
-
-    # python evaluate.py \
-    #     --data_sub_path "visa_step_random_nr05_tk1_tr60_seed${seed}" \
+    #     --data_sub_path "mvtec_step_random_nr10_tk1_tr60_seed${seed}" \
     #     --config_name "aatailedpatch_mvtec_01"
     
 done
+
+# # visa
+# for seed in {0..49}; do
+
+#     python evaluate.py \
+#         --data_sub_path "visa_pareto_random_nr05_seed${seed}" \
+#         --config_name "aatailedpatch_mvtec_05"
+
+#     # python evaluate.py \
+#     #     --data_sub_path "visa_step_random_nr05_tk4_tr60_seed${seed}" \
+#     #     --config_name "aatailedpatch_mvtec_01"
+
+#     # python evaluate.py \
+#     #     --data_sub_path "visa_step_random_nr05_tk1_tr60_seed${seed}" \
+#     #     --config_name "aatailedpatch_mvtec_01"
+    
+# done
