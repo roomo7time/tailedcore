3b45f5e8975d5385fe2b9fe367083436c2f42a71
jaewoo_park <park.jaewoo@aiv.ai>
2024-03-01 11:52:42 +0900

    feat: data making script update

--- run_make_visa.sh
+++ run_make_visa.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-for seed in {0..9}; do
+for seed in {0..99}; do
     python make_tailed_noisy_mvtec.py \
-        --source_dir "/home/jay/mnt/hdd01/data/image_datasets/anomaly_detection/visa" \
+        --data_name "visa" \
         --tail_type "step" \
         --noise_ratio 0.05 \
         --step_tail_k 4 \
+        --step_tail_class_ratio 0.6 \
+        --tail_level "random" \
         --seed ${seed}
 
     python make_tailed_noisy_mvtec.py \
-        --source_dir "/home/jay/mnt/hdd01/data/image_datasets/anomaly_detection/visa" \
+        --data_name "visa" \
         --tail_type "step" \
         --noise_ratio 0.05 \
         --step_tail_k 1 \
+        --step_tail_class_ratio 0.6 \
+        --tail_level "random" \
         --seed ${seed}
 
     python make_tailed_noisy_mvtec.py \
-        --source_dir "/home/jay/mnt/hdd01/data/image_datasets/anomaly_detection/visa" \
+        --data_name "visa" \
         --tail_type "pareto" \
         --noise_ratio 0.05 \
+        --tail_level "random" \
         --seed ${seed}
 done
