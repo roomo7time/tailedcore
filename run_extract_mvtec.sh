a4c74f2b52c9e3851cc05745b74024fdb9e08dea
jaewoo_park <park.jaewoo@aiv.ai>
2024-03-03 13:50:19 +0900

    fix: fix

--- run_extract_mvtec.sh
+++ run_extract_mvtec.sh
@@ -7,16 +7,16 @@ for seed in {101..105}; do
     # Define config names
     CONFIG_NAMES=(
         "extract_mvtec_01"
-        "extract_mvtec_02"
-        "extract_mvtec_03"
-        "extract_mvtec_04"
+        # "extract_mvtec_02"
+        # "extract_mvtec_03"
+        # "extract_mvtec_04"
     )
 
     # Define specific data names with their corresponding seeds
     DATA_NAMES=(
         "mvtec_step_random_nr10_tk1_tr60_seed${seed}"
         "mvtec_step_random_nr10_tk4_tr60_seed${seed}"
-        "mvtec_pareto_random_nr10_seed0"
+        "mvtec_pareto_random_nr10_seed_seed${seed}"
     )
 
     # Process each data name with each config name
