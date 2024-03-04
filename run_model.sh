5782b8369f35f97a65e001cd921ae4ec49bc4320
jaewoo_park <park.jaewoo@aiv.ai>
2024-03-04 19:23:33 +0900

    chore: chore

--- run_model.sh
+++ run_model.sh
@@ -1,36 +1,56 @@
 
 #!/bin/bash
 
+seeds=(
+    200
+    201
+    206
+    212
+    213
+    214
+    220
+    221
+    223
+    224
+    232
+    236
+    237
+    238
+    241
+    245
+    247
+)
+
 # mvtec
-for seed in {101..105}; do
+# for seed in "${seeds[@]}"; do
 
-    python evaluate.py \
-        --data_sub_path "mvtec_step_random_nr10_tk4_tr60_seed${seed}" \
-        --config_name "aatailedpatch_mvtec_09"
+#     # python evaluate.py \
+#     #     --data_sub_path "mvtec_step_random_nr10_tk4_tr60_seed${seed}" \
+#     #     --config_name "aatailedpatch_mvtec_09"
 
-    python evaluate.py \
-        --data_sub_path "mvtec_step_random_nr10_tk1_tr60_seed${seed}" \
-        --config_name "aatailedpatch_mvtec_09"
+#     # python evaluate.py \
+#     #     --data_sub_path "mvtec_step_random_nr10_tk1_tr60_seed${seed}" \
+#     #     --config_name "aatailedpatch_mvtec_09"
     
-    python evaluate.py \
-        --data_sub_path "mvtec_pareto_random_nr10_seed${seed}" \
-        --config_name "aatailedpatch_mvtec_10"
-    
-done
-
-# # visa
-# for seed in {200..299}; do
-
 #     python evaluate.py \
-#         --data_sub_path "visa_pareto_random_nr05_seed${seed}" \
-#         --config_name "aatailedpatch_mvtec_07"
-
-#     python evaluate.py \
-#         --data_sub_path "visa_step_random_nr05_tk1_tr60_seed${seed}" \
-#         --config_name "aatailedpatch_mvtec_07"
-
-#     python evaluate.py \
-#         --data_sub_path "visa_step_random_nr05_tk4_tr60_seed${seed}" \
-#         --config_name "aatailedpatch_mvtec_07"
+#         --data_sub_path "mvtec_pareto_random_nr10_seed${seed}" \
+#         --config_name "aatailedpatch_mvtec_10"
     
 # done
+
+# visa
+for seed in "${seeds[@]}"; do
+
+    # python evaluate.py \
+    #     --data_sub_path "visa_pareto_random_nr05_seed${seed}" \
+    #     --config_name "aatailedpatch_mvtec_11"
+
+    python evaluate.py \
+        --data_sub_path "visa_step_random_nr05_tk1_tr60_seed${seed}" \
+        --config_name "aatailedpatch_mvtec_11"
+
+    # python evaluate.py \
+    #     --data_sub_path "visa_step_random_nr05_tk4_tr60_seed${seed}" \
+    #     --config_name "aatailedpatch_mvtec_07"
+    
+done
