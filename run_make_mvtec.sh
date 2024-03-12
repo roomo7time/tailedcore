d240349fd8df19b67f3858c47efb0024a7ff8a76
jaewoo_park <park.jaewoo@aiv.ai>
2024-03-12 11:22:20 +0900

    fix: fix

--- run_make_mvtec.sh
+++ run_make_mvtec.sh
@@ -4,7 +4,7 @@ for seed in {101..105}; do
     python make_tailed_noisy_mvtec.py \
         --data_name "mvtec" \
         --tail_type "step" \
-        --noise_ratio 0.1 \
+        --noise_ratio 0.00 \
         --step_tail_k 4 \
         --step_tail_class_ratio 0.6 \
         --tail_level "random" \
@@ -13,7 +13,7 @@ for seed in {101..105}; do
     python make_tailed_noisy_mvtec.py \
         --data_name "mvtec" \
         --tail_type "step" \
-        --noise_ratio 0.1 \
+        --noise_ratio 0.00 \
         --step_tail_k 1 \
         --step_tail_class_ratio 0.6 \
         --tail_level "random" \
@@ -22,7 +22,7 @@ for seed in {101..105}; do
     python make_tailed_noisy_mvtec.py \
         --data_name "mvtec" \
         --tail_type "pareto" \
-        --noise_ratio 0.1 \
+        --noise_ratio 0.00 \
         --tail_level "random" \
         --seed ${seed}
 done
--- run_model.sh
+++ run_model.sh
@@ -8,23 +8,41 @@ seeds=(
     104
     105
 )
+# mvtec - ours: 9, 10 patchcore: 12
+# visa - ours: 11 patchcore: 12
 
-# mvtec
-for seed in "${seeds[@]}"; do
+noise_ratios=(05 40 00)
 
-    python evaluate.py \
-        --data_sub_path "mvtec_step_random_nr10_tk4_tr60_seed${seed}" \
-        --config_name "aatailedpatch_mvtec_13"
+for nr in "${noise_ratios[@]}"; do
+    for seed in "${seeds[@]}"; do
+        python evaluate.py \
+            --data_sub_path "mvtec_step_random_nr${nr}_tk4_tr60_seed${seed}" \
+            --config_name "aatailedpatch_mvtec_09"
 
-    python evaluate.py \
-        --data_sub_path "mvtec_step_random_nr10_tk1_tr60_seed${seed}" \
-        --config_name "aatailedpatch_mvtec_13"
-    
-    python evaluate.py \
-        --data_sub_path "mvtec_pareto_random_nr10_seed${seed}" \
-        --config_name "aatailedpatch_mvtec_13"
-    
+        python evaluate.py \
+            --data_sub_path "mvtec_step_random_nr${nr}_tk1_tr60_seed${seed}" \
+            --config_name "aatailedpatch_mvtec_09"
+
+        python evaluate.py \
+            --data_sub_path "mvtec_pareto_random_nr${nr}_seed${seed}" \
+            --config_name "aatailedpatch_mvtec_10"
+    done
 done
+
+# # mvtec - 
+# for seed in "${seeds[@]}"; do
+#     python evaluate.py \
+#         --data_sub_path "mvtec_step_random_nr05_tk4_tr60_seed${seed}" \
+#         --config_name "aatailedpatch_mvtec_9"
+
+#     python evaluate.py \
+#         --data_sub_path "mvtec_step_random_nr05_tk1_tr60_seed${seed}" \
+#         --config_name "aatailedpatch_mvtec_9"
+    
+#     python evaluate.py \
+#         --data_sub_path "mvtec_pareto_random_nr05_seed${seed}" \
+#         --config_name "aatailedpatch_mvtec_10"
+# done
 
 # # visa
 # for seed in "${seeds[@]}"; do
