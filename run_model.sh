054ebaf3b37f738cc58d3b5249ee6eec44851451
jaewoo_park <park.jaewoo@aiv.ai>
2024-03-14 03:18:58 +0900

    chore: chore

--- run_model.sh
+++ run_model.sh
@@ -11,7 +11,7 @@ seeds=(
 # mvtec - ours: 9, 10 patchcore: 12
 # visa - ours: 11 patchcore: 12
 
-noise_ratios=(05 40 00)
+noise_ratios=(0 5 10 40)
 
 # for nr in "${noise_ratios[@]}"; do
 #     for seed in "${seeds[@]}"; do
@@ -33,17 +33,33 @@ for nr in "${noise_ratios[@]}"; do
     for seed in "${seeds[@]}"; do
         python evaluate.py \
             --data_sub_path "mvtec_step_random_nr${nr}_tk4_tr60_seed${seed}" \
-            --config_name "softpatch_mvtec_01"
+            --config_name "aatailedpatch_mvtec_12"
 
         python evaluate.py \
             --data_sub_path "mvtec_step_random_nr${nr}_tk1_tr60_seed${seed}" \
-            --config_name "softpatch_mvtec_01"
+            --config_name "aatailedpatch_mvtec_12"
 
         python evaluate.py \
             --data_sub_path "mvtec_pareto_random_nr${nr}_seed${seed}" \
-            --config_name "softpatch_mvtec_01"
+            --config_name "aatailedpatch_mvtec_12"
     done
 done
+
+# for nr in "${noise_ratios[@]}"; do
+#     for seed in "${seeds[@]}"; do
+#         python evaluate.py \
+#             --data_sub_path "mvtec_step_random_nr${nr}_tk4_tr60_seed${seed}" \
+#             --config_name "softpatch_mvtec_01"
+
+#         python evaluate.py \
+#             --data_sub_path "mvtec_step_random_nr${nr}_tk1_tr60_seed${seed}" \
+#             --config_name "softpatch_mvtec_01"
+
+#         python evaluate.py \
+#             --data_sub_path "mvtec_pareto_random_nr${nr}_seed${seed}" \
+#             --config_name "softpatch_mvtec_01"
+#     done
+# done
 
 # # mvtec - 
 # for seed in "${seeds[@]}"; do
